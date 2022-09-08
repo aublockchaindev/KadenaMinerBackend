99c57194ec9b8af52dfa3a54be55551c4d9545ac
aublockchaindev <110176446+aublockchaindev@users.noreply.github.com>
2022-09-08 23:40:42 +1000

    Update Index.js

--- Index.js
+++ Index.js
@@ -25,7 +25,7 @@ const mintContractService = async (req) => {
     console.log("I am here" + req);
     //const mintInput = JSON.parse(req);
     const mintInput = req.body;
-    console.log("I am here" + mintInput);
+    console.log("I am here" + mintInput.ownerAddress);
     const creationTime = () => Math.round((new Date).getTime() / 1000);
     //const createdDate = new Date(mintInput.created-date);
     console.log("time created" + mintInput.ownerAddress);
